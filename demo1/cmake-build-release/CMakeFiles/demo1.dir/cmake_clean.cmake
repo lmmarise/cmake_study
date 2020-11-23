4028fe01a19af2420274167d03b593821d1cccf5
tsb <tsb.j@qq.com>
2020-11-23 23:17:54 +0800

    init

---
+++ demo1/cmake-build-release/CMakeFiles/demo1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo1.dir/main.cpp.o"
+  "demo1.exe"
+  "demo1.pdb"
+  "libdemo1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
